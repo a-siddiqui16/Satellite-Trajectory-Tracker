7269585c5c3214e1af639807cd6865ba36613957
Abdur-Rahman <mailabdurrahmans@gmail.com>
2025-11-27 13:31:47 +0000

    Implement TLE parser for satellite data extraction
    Added a TLE parser function to extract satellite parameters from TLE lines.

--- satellite/tle_fetcher.py
+++ satellite/tle_fetcher.py
@@ -17,9 +17,48 @@ def fetch_satellite_tle(norad_id):
         tle_line2 = lines[2].strip()
 
         return norad_id, satellite_name, tle_line1, tle_line2
-    
+   
     else:
         print(response.status_code)
 
 
-print(fetch_satellite_tle(norad_id=25544))
+
+#Line 1: 1 25544U 98067A   22001.74462497  .00001435  00000-0  34779-4 0  9992
+#Line 2: 2 25544  51.6464  24.2704 0004064  69.5467 290.6355 15.48835264296862
+
+
+def tle_parser(tle_line1, tle_line2):
+
+    #Extracting TLE Parameters and putting them into a dictionary
+
+    return {
+
+        #Line 1
+        "satellite_number": tle_line1[2:7].strip(),
+        "classification": tle_line1[7].strip(),
+        "international_designator": tle_line1[9:17].strip(),
+        "epoch_year": tle_line1[18:20].strip(),
+        "epoch_day": tle_line1[20:32].strip(),
+        "first_derivative": tle_line1[33:43].strip(),
+        "second_derivative": tle_line1[44:52].strip(),
+        "bstar_drag": tle_line1[53:61].strip(),
+        "ephemeris_type": tle_line1[62].strip(),
+        "element_number": tle_line1[64:68].strip(),
+
+        #Line 2
+        "inclination": tle_line2[8:16].strip(),
+        "right_ascension": tle_line2[17:25].strip(),
+        "eccentricity": tle_line2[26:33].strip(),
+        "argument_of_perigee": tle_line2[34:42].strip(),
+        "mean_anomaly": tle_line2[43:51].strip(),
+        "mean_motion": tle_line2[52:63].strip(),
+        "revolution_number": tle_line2[63:68].strip()
+
+    }
+
+norad_id = norad_id, name, line1, line2 = fetch_satellite_tle(25544)
+
+parameters = tle_parser(line1, line2)
+
+for key, value in parameters.items():
+    print(key, value)
