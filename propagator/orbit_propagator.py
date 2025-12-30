049c8f67d20e4ea6006e71d0cb5dab470ef1298a
Abdur-Rahman Siddiqui <mailabdurrahmans@gmail.com>
2025-12-30 10:59:24 +0000

    Fixed import issues

--- propagator/orbit_propagator.py
+++ propagator/orbit_propagator.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import ode
-import planetary_data
+from propagator import planetary_data
 import math
 
 pi = math.pi
@@ -22,7 +22,7 @@ class OrbitPropagator:
         self.n_steps = int(np.ceil(self.tspan / self.dt))
 
         #initialise variables   
-        self.ys = np.zeros((self.n_steps, 6)) #6 states, 60 rows wide by 6 columns
+        self.ys = np.zeros((self.n_steps, 6))
         self.ts = np.zeros((self.n_steps, 1))
 
         #intial conditions
@@ -32,54 +32,40 @@ class OrbitPropagator:
 
         #solver
         self.solver = ode(self.diffy_q)
-        self.solver.set_integrator('lsoda') #fastest ODE according to the tutorial
+        self.solver.set_integrator('lsoda')
         self.solver.set_initial_value(self.y0, 0)
 
         #propagate orbit
-
         while self.solver.successful() and self.step < self.n_steps:
             self.solver.integrate(self.solver.t + self.dt)
-            self.ts[self.step] = self.solver.t #step will be initially set to one, so use a variable to not override the conditons
+            self.ts[self.step] = self.solver.t
             self.ys[self.step] = self.solver.y
             self.step += 1
 
-        self.rs = self.ys[:, :3] #all the rows in column 1 and 2
+        self.rs = self.ys[:, :3]
         self.vs = self.ys[:,3:]
 
 
     def diffy_q(self, t, y):
-        #unpack the state
-        #t -> time
-        #y -> state
-        #mu -> additional parameter
-
         rx, ry, rz, vx, vy, vz = y
-        r = np.array([rx, ry, rz]) #Changes the format to a vector
+        r = np.array([rx, ry, rz])
 
-        #norm of radius vector
         norm_r = np.linalg.norm(r)
 
-        #two body acceleration
         ax = -self.cb['mu'] * rx / norm_r**3
         ay = -self.cb['mu'] * ry / norm_r**3
         az = -self.cb['mu'] * rz / norm_r**3
 
-        return [vx, vy, vz, ax, ay, az] #derivative of precision is your velocrit
-
-        #r is pointing from the centre of the earth to your
-        # satellite and the acceleration due to gravity is going in the opposite direction so r is negative
+        return [vx, vy, vz, ax, ay, az]
 
 
     def plot_3d(self, show_plot=False, save_plot=False, title="Test"):
-        #3D plot screen to be represented to the user
         fig = plt.figure(figsize=(10,10))
         ax = fig.add_subplot(111,projection='3d')
         
-        #plot trajectory and starting point
         ax.plot(self.rs[:,0], self.rs[:,1], self.rs[:,2], 'k-')
         ax.plot([self.rs[0,0]], [self.rs[0,1]], [self.rs[0,2]],'ko')
 
-        #plot earth
         r_plot = self.cb["radius"]
 
         u = np.linspace(0, 2 * np.pi, 50)
@@ -89,30 +75,19 @@ class OrbitPropagator:
         z = self.cb["radius"] * np.outer(np.ones(np.size(u)), np.cos(v))
         ax.plot_wireframe(x, y, z, color = "k", linewidth = 0.25)
 
-
-        #check for custom axes limits
-        max_val = np.max(np.abs(self.r))
-
-        #set labels
-        max_val = np.max(np.abs(self.r))
+        max_val = np.max(np.abs(self.rs))
         ax.set_xlim([-max_val, max_val])
         ax.set_ylim([-max_val, max_val])
         ax.set_zlim([-max_val, max_val])
 
-        #sets titles
         ax.set_xlabel('X (km)')
         ax.set_ylabel('Y (km)')
         ax.set_zlabel('Z (km)')
 
-        #ax.set_aspect('equal')
-
         ax.set_title(title)
         plt.legend()
-        plt.show()
 
         if show_plot:
             plt.show()
         if save_plot:
             plt.savefig(title+'.png', dpi=300)
-
-
