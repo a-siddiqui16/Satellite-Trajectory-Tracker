e3ee60e335e4938390fa151df95b8d713304b027
Abdur-Rahman Siddiqui <mailabdurrahmans@gmail.com>
2025-12-30 11:10:30 +0000

    3D Plot

--- propagator/orbit_propagator.py
+++ propagator/orbit_propagator.py
@@ -58,7 +58,7 @@ class OrbitPropagator:
         self.r = np.array([rx, ry, rz]) #Changes the format to a vector
 
         #norm of radius vector
-        norm_r = np.linalg.norm(self.rs)
+        norm_r = np.linalg.norm(self.r)
 
         #two body acceleration
         ax = -self.cb['mu'] * rx / norm_r**3
