008dd881fe0567de1ea11b4fd533b2cea63e4943
Abdur-Rahman Siddiqui <mailabdurrahmans@gmail.com>
2026-01-02 11:08:28 +0000

    Commenting out unecessary code

--- skyfield_calculations/tracker_visualisation.py
+++ skyfield_calculations/tracker_visualisation.py
@@ -1,82 +1,98 @@
-import sqlite3
-from vpython import *
-from skyfield.api import EarthSatellite, load, wgs84
-from tle_fetcher import fetch_satellite_tle, tle_parser
-from orbital_calculations import classify_orbit
-from datetime import datetime
+# import sqlite3
+# from vpython import *
+# from skyfield.api import EarthSatellite, load, wgs84
+# from tle_fetcher import fetch_satellite_tle, tle_parser
+# from orbital_calculations import classify_orbit
 
 
-db_path = "satellite_database.db"
+# db_path = "satellite_database.db"
 
-earth_radius_km = 6378
-satellite_size = 50
-trail_size = 10
-
-
-def calculate_speed(velocity):
-    return (velocity[0]**2 + velocity[1]**2 + velocity[2]**2) ** 0.5
+# earth_radius_km = 6378
+# satellite_size = 50
+# trail_size = 10
 
 
+# def calculate_speed(velocity):
+#     return (velocity[0]**2 + velocity[1]**2 + velocity[2]**2) ** 0.5
 
-def store_satellite_data(norad_id):
 
-    satellite_data = fetch_satellite_tle(norad_id)
-    if not satellite_data:
-        print("Could not fetch satellite data")
-        return False
+
+# def store_satellite_data(norad_id):
+
+#     satellite_data = fetch_satellite_tle(norad_id)
+#     if not satellite_data:
+#         print("Could not fetch satellite data")
+#         return False
     
-    norad_id, satellite_name, tle_line1, tle_line2 = satellite_data
-    satellite = EarthSatellite(tle_line1, tle_line2, satellite_name, ts)
-    orbit_type, altitude, inclination = classify_orbit(satellite)
+#     norad_id, satellite_name, tle_line1, tle_line2 = satellite_data
+#     satellite = EarthSatellite(tle_line1, tle_line2, satellite_name, ts)
+#     orbit_type, altitude, inclination = classify_orbit(satellite)
 
     
 
-    #Extracting values so I can put them in the database
-    data_parameters = tle_parser(tle_line1, tle_line2)
-    eccentricity = float("0." + data_parameters['eccentricity'])
-    mean_motion = float(data_parameters['mean_motion'])
-    epoch_day = data_parameters['epoch_day']
-    epoch_year = data_parameters['epoch_year']
+#     #Extracting values so I can put them in the database
+#     data_parameters = tle_parser(tle_line1, tle_line2)
+#     eccentricity = float("0." + data_parameters['eccentricity'])
+#     mean_motion = float(data_parameters['mean_motion'])
+#     epoch_day = data_parameters['epoch_day']
+#     epoch_year = data_parameters['epoch_year']
 
-    if int(epoch_year) < 57: #No satellites exist before 1957
-        full_year = 2000 + int(epoch_year)   #Example # 00-56 → 2000-2056
-    else:
-        full_year = 1900 + int(epoch_year)   #Example 57-99 → 1957-1999
+#     if int(epoch_year) < 57: #No satellites exist before 1957
+#         full_year = 2000 + int(epoch_year)   #Example # 00-56 → 2000-2056
+#     else:
+#         full_year = 1900 + int(epoch_year)   #Example 57-99 → 1957-1999
     
-    epoch_date = f"{full_year}-{epoch_day}"
+#     epoch_date = f"{full_year}-{epoch_day}"
 
 
-    try:
-        with sqlite3.connect(db_path) as conn:
+#     try:
+#         with sqlite3.connect(db_path) as conn:
 
-            c = conn.cursor()
+#             c = conn.cursor()
 
-            c.execute("""INSERT OR IGNORE INTO Satellites
-                    (norad_id, satellite_name, satellite_type)
-                    VALUES (?, ?, ?)""",
-                    (norad_id, satellite_name, None))
+#             c.execute("""INSERT OR IGNORE INTO Satellites
+#                     (norad_id, satellite_name, satellite_type)
+#                     VALUES (?, ?, ?)""",
+#                     (norad_id, satellite_name, None))
             
-            c.execute("""INSERT INTO TLE_Data 
-                        (norad_id, tle_line1, tle_line2, orbit_type, 
-                        inclination, eccentricity, mean_motion, epoch_date)
-                        VALUES (?, ?, ?, ?, ?, ?, ?, ?)""",
-                    (norad_id, tle_line1, tle_line2, orbit_type,
-                    inclination, eccentricity, mean_motion, epoch_date))
+#             c.execute("""INSERT INTO TLE_Data 
+#                         (norad_id, tle_line1, tle_line2, orbit_type, 
+#                         inclination, eccentricity, mean_motion, epoch_date)
+#                         VALUES (?, ?, ?, ?, ?, ?, ?, ?)""",
+#                     (norad_id, tle_line1, tle_line2, orbit_type,
+#                     inclination, eccentricity, mean_motion, epoch_date))
             
-            conn.commit()
+#             conn.commit()
 
-        return True
+#         return True
     
-    except sqlite3.Error as e:
-        print("Error")
-        return False
+#     except sqlite3.Error as e:
+#         print("Error")
+#         return False
 
 
-ts = load.timescale()
-norad_id = int(input("ENTER NORAD ID: "))
-satellite_data = fetch_satellite_tle(norad_id)
+# ts = load.timescale()
+# norad_id = int(input("ENTER NORAD ID: "))
+# satellite_data = fetch_satellite_tle(norad_id)
 
-store_satellite_data(norad_id)
+# store_satellite_data(norad_id)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # if satellite_data:
 
